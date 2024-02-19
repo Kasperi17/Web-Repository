deac2618a2e8cf7f072abd3b887efa360847c27c
Kasperi Salonen <AD4449@student.jamk.fi>
2024-02-19 21:04:26 +0200

    Javascript

--- Java/Tehtävä - Aritmeettiset operaattorit.js
+++ Java/Tehtävä - Aritmeettiset operaattorit.js
@@ -1,6 +1,2 @@
-var omena = 69;
-var alennusprosentti = 25;
-
-var alennus = omena - (omena * alennusprosentti / 100)
-
-console.log(alennus)
+const hobbies = ['uinti', 'lukeminen', 'leipominen'];
+console.log(hobbies)
